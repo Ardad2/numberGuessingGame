b075d737db919e52a3e1cfcd89bf12f3d03e6f44
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-02 20:38:04 -0700

    imported the buttons correctly

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,3 +1,14 @@
-function StartGameScreen() {}
+import { TextInput, View } from 'react-native';
+
+import PrimaryButton from '../components/PrimaryButton'
+
+function StartGameScreen() {
+    return (
+        <View>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+    );
+}
 
 export default StartGameScreen;
