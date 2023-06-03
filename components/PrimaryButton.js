02da9e175add38e936918557f9357f2e0d3998a1
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-02 20:59:32 -0700

    fixed color of the button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,4 +1,4 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({children}) {
 
@@ -7,14 +7,39 @@ function PrimaryButton({children}) {
     }
 
     return (
-        <Pressable onPress={pressHandler}>
-        <View>
-            <Text>
+        <View style={styles.buttonOuterContainer}>
+        <Pressable 
+        style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed]: styles.buttonInnerContainer }
+        onPress={pressHandler} 
+        android_ripple = {{color: '#640233'}}
+        >
+            <Text style ={styles.buttonText}>
                 {children}
             </Text>
-        </View>
         </Pressable>
+        </View>
     );
 }
 
 export default PrimaryButton
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
+    }
+})
