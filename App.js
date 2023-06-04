66cd856a0e471762cc367a1e9dd00840bb9d51c5
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-03 20:22:38 -0700

    transparent background

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -7,9 +7,15 @@ import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
   return (
    <LinearGradient
-   // Background Linear Gradient
    colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+     <ImageBackground s
+     ource={require('./assets/images/background.png')} 
+     resizeMode="cover" 
+     style={styles.rootScreen} 
+     imageStyle={styles.backgroundImage}
+     >
   <StartGameScreen />
+  </ImageBackground>
   </LinearGradient> 
   );
 }
@@ -17,5 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity:0.15,
   }
 });
