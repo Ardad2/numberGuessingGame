46b6f120330248797c3a489bd07d57fc021d1689
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-03 20:29:45 -0700

    added handlers for both the buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput, StyleSheet, View} from 'react-native';
+import { TextInput, StyleSheet, View, Alert} from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton'
 
@@ -10,8 +10,20 @@ function StartGameScreen() {
         setEnteredNumber(enteredText);
     }
 
-    function confirmInputHandler() {
+    function resetInputHandler() {
+        setEnteredNumber('');
+    }
 
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber);
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert(
+                'Invalid number!', 
+                'Number has to be a number between 1 and 99',
+                [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler}]
+            )
+         }
     }
 
     return (
